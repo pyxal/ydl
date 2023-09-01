5804b2e7363f00e35b127189036dcf74e08f2aa6
pyxal <31687737+pyxal@users.noreply.github.com>
2023-09-01 09:48:20 +0200

    Add files via upload

--- ydl.py
+++ ydl.py
@@ -2,36 +2,43 @@
 
 #
 # yt-dlp with time specification using ffmpeg
-# requires yt-dlp, ffmpeg
 #
-# Example: python3 ydl.py ytLink 96 125
-#
-# 96 is start second, 125 is length of clip in seconds
-# cuts out a clip of the video from 1:36 to 3:41
-#
-
 
 # imports
 from sys import argv
 from os import system as sc
 
-# sec to min
-def secToMin(secs):
-    min = str(int(secs) // 60).zfill(2)
-    sec = str(int(secs) % 60).zfill(2)
-    return f'{min}:{sec}'
+# time format
+def tf(time):
+    if len(time) == 1: return f'00:00:0{time}'
+    elif len(time) == 2: return f'00:00:{time}'
+    elif len(time) == 3: return f'00:0{time[0]}:{time[1:3]}'
+    elif len(time) == 4: return f'00:{time[0:2]}:{time[2:4]}'
+    elif len(time) == 5: return f'0{time[0]}:{time[1:3]}:{time[3:5]}'
+    elif len(time) == 6: return f'{time[0:2]}:{time[2:4]}:{time[4:6]}'
+
+# set time selection
+if len(argv) > 2:
+    startTime = tf(argv[2])
+    endTime = '12:00:00'
+    if len(argv) > 3:
+        endTime = tf(argv[3])
+        print(f"dl from {startTime} to {endTime}")
+    else: print(f"dl from {startTime} to end")
+
 
 # download
 sc(f'yt-dlp --no-mtime {argv[1]}')
 
 # cut to specified time
-if len(argv) == 4:
+if len(argv) > 2:
     sc(f"""
-    echo executing ffmpeg... &&
+    echo running ffmpeg... &&
     fileName=$(ls -t | head -n 1 | sed -e 's/\.[^.]*$//') &&
-    ffmpeg -hide_banner -loglevel error -ss 0:{secToMin(argv[2])}.00 -i "$(ls -t | head -n 1)" -t 0:{secToMin(argv[3])}.00 -c copy temp.mp4 &&
+    ffmpeg -hide_banner -loglevel error -i "$(ls -t | head -n 1)" -ss {startTime} -to {endTime} -c copy temp.mp4 &&
     rm "$(ls -t | head -n 2 | tail -n 1)" &&
     mv temp.mp4 "$fileName".mp4 &&
     echo Done
     """)
+
 
